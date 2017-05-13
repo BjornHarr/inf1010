f4c69c9d5cb7c7f267cff684321a99efd8704661
BjornHarr <bjornharr@gmail.com>
2017-05-13 22:20:03 +0200

    GridPane

--- oblig7/Oblig7.java
+++ oblig7/Oblig7.java
@@ -1,15 +1,28 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
 
 public class Oblig7 extends Application{
 
     @Override
     public void start(Stage window) throws Exception {
-        Scene scene = new Scene(new Pane(), 1080, 560);
-        window.setScene(scene);
         window.setTitle("bjorhhar - oblig7");
+
+        GridPane tabell = new GridPane();
+
+        for (int i = 1; i < 4; i++){
+            for (int j = 1; j < 4; j++){
+                tabell.add(new Button("Button" + i*j), j, i, 1, 1);
+            }
+        }
+
+        tabell.setHgap(10);
+        tabell.setVgap(10);
+
+        Scene textScene = new Scene(tabell, 240, 120);
+        window.setScene(textScene);
         window.show();
     }
 
